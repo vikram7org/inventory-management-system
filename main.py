49d620affc42d838900f75e3524dd4f192de418e
vikram-singh9 <163000375+vikram-singh9@users.noreply.github.com>
2025-07-06 02:49:33 +0500

    Update main.py
    Co-Authored-By: Rayan <209516406+rayan904@users.noreply.github.com>
    Co-Authored-By: Ismailabubakar <94026692+ismailabubakar@users.noreply.github.com>

--- main.py
+++ main.py
@@ -8,7 +8,7 @@ class Inventory:
         else:
             self.items[name] = quantity
         print(f"Item '{name}' added to inventory.")
-#wdsdnsnn
+
 
     def remove_item(self, name):
         if name in self.items:
